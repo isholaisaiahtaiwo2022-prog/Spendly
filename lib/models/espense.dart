08e14e576c83f816ae07e9113b8a91cd7f37debb
isholaisaiahtaiwo2022-prog <isholaisaiahtaiwo2022@gmail.com>
2026-09-09 22:24:27 +0100

    Define expense data model

---
+++ lib/models/espense.dart
@@ -0,0 +1,22 @@
+enum TransactionType { expense, income }
+
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final String category;
+  final DateTime date;
+  final TransactionType type;
+  final String? paymentMethod;
+
+
+  const Expense ({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+    this.type = TransactionType.expense,
+    this.paymentMethod,
+  })
+}
